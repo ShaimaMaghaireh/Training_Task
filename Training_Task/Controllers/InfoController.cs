263d5b83032408649e684569e88e0a5f621030fc
ShaimaMaghaireh <salmaghaireh@gmail.com>
2025-05-21 15:54:13 +0300

    Add project files.

---
+++ Training_Task/Controllers/InfoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Training_Task.Models;
+
+namespace Training_Task.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info
+        public ActionResult Index()
+        {
+            Info myInfo = new Info();
+            myInfo.Name = "Shaima Maghaireh";
+            myInfo.University = "Ajloun National University";
+            myInfo.Major = "Software Engineering";
+            myInfo.Course = "Field Training";
+            myInfo.Age = 21;
+            myInfo.Skills = "ASP.NET, C#, HTML, CSS";
+            myInfo.Email = "example@gmail.com";
+            myInfo.City = "Irbid";
+            return View(myInfo);
+        }
+    }
+}
---
+++ Training_Task/Models/Info.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Training_Task.Models
+{
+    public class Info
+    {
+        public string Name { get; set; }
+        public string University { get; set; }
+        public string Major { get; set; }
+        public string Course { get; set; }
+        public int Age { get; set; }
+        public string Skills { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+    }
+}
