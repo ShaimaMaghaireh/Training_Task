7096abca0e9629a080e85193818987630c030204
ShaimaMaghaireh <salmaghaireh@gmail.com>
2025-05-26 13:58:45 +0300

    commit

---
+++ Training_Task/Controllers/FormController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcFormExample.Models;
+
+namespace Training_Task.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveData(UserInputViewModel model)
+        {
+            // Pass the data to the new page (view)
+            return View("DisplayData", model);
+        }
+    }
+}
---
+++ Training_Task/Models/UserInputViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcFormExample.Models
+{
+    public class UserInputViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
